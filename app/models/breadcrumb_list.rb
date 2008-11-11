b1c2e2cb39015ddd1219b9ab940d7d73d558986e
p-naef <pascal@p-naefs-imac.local>
2008-11-11 17:16:31 +0100

    Added separator

--- app/models/breadcrumb_list.rb
+++ app/models/breadcrumb_list.rb
@@ -6,18 +6,24 @@ module BreadcrumbList
     Renders a trail of breadcrumbs to the current page, rendered as an undordered list. The id and class attributes specify the HTML id and HTML class, respectively, of the containing unordered list. The boolean nolinks attribute can be specified to render breadcrumbs in plain text, without any links (useful when generating title tag).
 
     *Usage:*
-    <pre><code><r:breadcrumb_list [nolinks="true"] [id="id"] [class="class"] /></code></pre>
+    <pre><code><r:breadcrumb_list [nolinks="true"] [id="id"] [class="class"] [separator="/"] /></code></pre>
   }
   tag 'breadcrumb_list' do |tag|
     page = tag.locals.page
-    breadcrumbs = [page.breadcrumb]
+    separator = tag.attr['separator']
+    validSeparator = separator != nil && separator != ""
+    breadcrumbs =  validSeparator ? [ separator + " " + page.breadcrumb] : [page.breadcrumb]
     nolinks = (tag.attr['nolinks'] == 'true')
     page.ancestors.each do |ancestor|
       tag.locals.page = ancestor
       if nolinks
-        breadcrumbs.unshift tag.render('breadcrumb')
+        breadcrumbs.unshift validSeparator ? separator + " " + tag.render('breadcrumb') : "" + tag.render('breadcrumb')
       else
-        breadcrumbs.unshift %{<a href="#{tag.render('url')}">#{tag.render('breadcrumb')}</a>}
+        if validSeparator
+          breadcrumbs.unshift %{#{separator} <a href="#{tag.render('url')}">#{tag.render('breadcrumb')}</a>}
+        else
+          breadcrumbs.unshift %{<a href="#{tag.render('url')}">#{tag.render('breadcrumb')}</a>}
+        end
       end
     end
     content_tag(:ul, breadcrumbs.map {|breadcrumb| content_tag(:li, breadcrumb)}, { :id => tag.attr['id'], :class => tag.attr['class'] })
